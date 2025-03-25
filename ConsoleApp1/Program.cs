9366a97a4072c8e572f3e6e229859d016724deaf
Paramdeep Singh <prmdpsn56@gmail.com>
2025-03-24 22:00:55 -0700

    Making statinc method

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Coding.Exercise
+{
+   public class Program
+    {
+        public static void CalculateAverage()
+        {
+            int num1 = 10;
+            int num2 = 20;
+            int num3 = 30;
+            int average = (num1 + num2 + num3) / 3;
+            Console.WriteLine("The average is: \n " + average);
+            System.Console.WriteLine("Hello \n \t world");
+        }
+
+        public static void Main()
+        {
+            CalculateAverage(); // Directly calling without an instance
+        }
+    }
+}
