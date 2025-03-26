1dd6a76d198bc81851d291f1bcf0d62eaf08b73d
Paramdeep Singh <prmdpsn56@gmail.com>
2025-03-25 21:48:31 -0700

    Methods

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,20 +1,36 @@
-﻿namespace Coding.Exercise
-{
-   public class Program
-    {
-        public static void CalculateAverage()
-        {
-            int num1 = 10;
-            int num2 = 20;
-            int num3 = 30;
-            int average = (num1 + num2 + num3) / 3;
-            Console.WriteLine("The average is: \n " + average);
-            System.Console.WriteLine("Hello \n \t world");
-        }
+﻿int [] array1 = new int[5];
+array1 = [1,2,3,4,5];
 
-        public static void Main()
-        {
-            CalculateAverage(); // Directly calling without an instance
-        }
-    }
+int [] array2 = [1,2,3,4,5,6];
+int [,] array2d = {{1,2},{10,12}};
+string [,] array22d = {
+    {"x","x","x"},
+    {"x","o","x"},
+    {"x","x","x"}
+    };
+
+int [][] jaggedArray = [[12,3],[1,2,3,4,4]];
+
+string [] weekdays = ["mon","tue","wed","thu","fri","sat","sun"];
+
+// System.Console.WriteLine(array1[0]);
+// System.Console.WriteLine(array2[5]);
+// System.Console.WriteLine(weekdays[2]);
+// System.Console.WriteLine("length of weekdays array is " + weekdays.Length);
+
+
+// for(int i = 0; i < weekdays.Length; i++){
+//     System.Console.WriteLine(weekdays[i]);
+// }
+
+// foreach(var day in weekdays){
+//     System.Console.WriteLine(day);
+// }
+
+
+foreach(var item in array22d){
+    System.Console.WriteLine(item);
 }
+
+// System.Console.WriteLine(array2d[1,1]);
+// System.Console.WriteLine(jaggedArray[1][1]);
--- ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85b6634bf7e68786e4a694f0f735155b1a8eda82")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9366a97a4072c8e572f3e6e229859d016724deaf")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
