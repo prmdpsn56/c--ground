9555f4e284550e5c8d949bd427fce3dc48e0ed8d
Paramdeep Singh <prmdpsn56@gmail.com>
2025-03-27 22:09:41 -0700

    using static and non static methods

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,12 +1,32 @@
 ﻿using System;
+using System.Security.Cryptography.X509Certificates;
 
 namespace MyApp
-{
-    internal class Program
+{    internal class Program
     {
         static void Main()
         {
-            Console.WriteLine("Hello World!");
+
+           int myResult;
+           int myResult2;
+
+            Program programClassInstance = new Program();
+            myResult2 = programClassInstance.subtractTwoValues(20,10);
+            myResult = AddTwoValues(1,1);
+
+             System.Console.WriteLine(myResult);
+             System.Console.WriteLine(myResult2);
+          
+        }
+
+        static int AddTwoValues(int numb1, int numb2){
+            return numb1 + numb2;
+        }
+
+
+        public int subtractTwoValues(int num1, int num2){
+            return num1 = num2;
+
         }
     }
 }
