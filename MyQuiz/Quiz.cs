7939c556f0bc961e6a12d270095649c66441a0dc
Paramdeep Singh <prmdpsn56@gmail.com>
2025-04-05 15:39:04 -0700

    Updated UI for better display

--- MyQuiz/Quiz.cs
+++ MyQuiz/Quiz.cs
@@ -1,16 +1,21 @@
 using System;
+using System.Drawing;
 using System.Reflection.PortableExecutable;
+using System.Runtime.InteropServices;
 
 namespace MyQuiz;
 
 public class Quiz
 {
 
-    public Question[] questions;
+    private Question[] _questions;
+    private int _score;
+    
 
     public Quiz( Question[] questions)
     {
-        this.questions = questions;
+        this._questions = questions;
+        _score = 0;
     }
 
     
@@ -18,7 +23,7 @@ public class Quiz
         System.Console.WriteLine("Welcome to the Quiz!");
         int questionNumber = 1;
 
-        foreach (var question in questions)
+        foreach (var question in _questions)
         {
             int userChoice = 0;
             Console.WriteLine($"Question:  {questionNumber++}");
@@ -26,10 +31,13 @@ public class Quiz
             userChoice = GetUserChoice();
             if(question.IsCorrectAnswer(userChoice)){
                 System.Console.WriteLine("correct!");
+                _score++;
             }else{
                 System.Console.WriteLine($"wrong! the correct answer is :{question.Answers[question.CorrectAnswerIndex]}");
             }
         }
+
+        this.DisplayResult();
     }
 
     private void DisplayQuestion (Question question){
@@ -56,5 +64,21 @@ public class Quiz
             input = Console.ReadLine();
         }
         return choice - 1; //adjust to zerio indexing
+    }
+
+
+    private void DisplayResult(){
+        System.Console.WriteLine("Quiz finished and your score is as below");
+        System.Console.WriteLine($"Your Score is _ {_score} out of {_questions.Length}");
+
+
+        double percentage = ((double)_score / _questions.Length) * 100;
+
+        if(percentage > 50 ){
+            Console.ForegroundColor = ConsoleColor.Green;
+        }else{
+            Console.ForegroundColor = ConsoleColor.Red;
+        }
+        System.Console.WriteLine($"Your Scoring percentage is {percentage}");
     }
 }
