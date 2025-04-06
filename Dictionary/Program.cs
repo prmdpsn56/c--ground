b4bfc91a9152c42d2d875fc8aa70f6caf2795bb1
Paramdeep Singh <prmdpsn56@gmail.com>
2025-04-06 13:48:31 -0700

    Adding removing and for each on Dictionary

---
+++ Dictionary/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<int, string> employees = new Dictionary<int, string>();
+            employees.Add(1,"Param");
+            employees.Add(2,"John");
+            employees.Add(3,"Mohn");
+            employees.Add(4,"Ram");
+
+//get
+            string name = employees[1];
+            // System.Console.WriteLine(name);
+
+//set       
+            employees[2] = "Singh";
+            // System.Console.WriteLine(name);
+
+//remove    
+            employees.Remove(1);
+
+//for loop
+            foreach (KeyValuePair<int,string> item in employees)
+            {
+                System.Console.WriteLine($"{item.Key} : {item.Value}");
+            }
+
+        }
+    }
+}
