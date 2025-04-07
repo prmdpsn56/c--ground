b3dde714e277d74643f9ed9659b9410945a21c28
Paramdeep Singh <prmdpsn56@gmail.com>
2025-04-06 17:25:05 -0700

    Added Employees

--- Dictionary/Program.cs
+++ Dictionary/Program.cs
@@ -20,8 +20,22 @@ namespace MyApp
             employees[2] = "Singh";
             // System.Console.WriteLine(name);
 
+//adding at an existing item.
+            employees.Add(1,"John Doe");
+            //we will get a runtime error when we try to add at the same place, we can just update based on if there is a value already or not.
+            if(!employees.ContainsKey(1)){
+                employees.Add(1,"John doe");
+            }
+
+            var counter = 101;
+            while (!employees.ContainsKey(counter))
+            {
+                counter++;
+            }
+            employees.Add(counter,"Jesus christ");
+            
 //remove    
-            employees.Remove(1);
+            // employees.Remove(1);
 
 //for loop
             foreach (KeyValuePair<int,string> item in employees)
