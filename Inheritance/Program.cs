2156fbd671e9ae5a90b533f4765866a359539860
Paramdeep Singh <prmdpsn56@gmail.com>
2025-04-12 16:01:53 -0700

    base class inheritance

--- Inheritance/Program.cs
+++ Inheritance/Program.cs
@@ -4,9 +4,35 @@ namespace MyApp
 {
     internal class Program
     {
+       
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Dog animal = new Dog();
+            animal.Bark();
+        }
+    }
+
+    class Animal {
+
+        public string _publicField;
+        public string PublicFields { get; set; }
+        private string PrivateFields { get; set; }
+        protected string ProtectedFields { get; set; }
+        
+        public void Eat(){
+            Console.WriteLine("Eating...!!");
+        }
+    }
+
+
+    class Dog: Animal {
+        public void Bark() {
+            Console.WriteLine("Barking...!!");
+            PublicFields = "Woff!!";
+            _publicField = "woff again!!";
+            Console.Write(PublicFields+_publicField);
+
         }
     }
 }
