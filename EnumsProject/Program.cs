e29369add5b34d689c73f7b433fb0059ba778d7a
Paramdeep Singh <prmdpsn56@gmail.com>
2025-05-04 13:34:47 -0700

    brief examples

---
+++ EnumsProject/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using EnumsProject.enums;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Days.Sunday); // Output: Sunday
+            Console.WriteLine((int)Days.Sunday); // Output: 1
+        }
+    }
+}
---
+++ EnumsProject/enums/Days.cs
@@ -0,0 +1,13 @@
+namespace EnumsProject.enums;
+
+enum Days
+{
+    Sunday,
+    Monday,
+    Tuesday,
+    Wednesday,
+    Thursday,
+    Friday,
+    Saturday
+}
+
